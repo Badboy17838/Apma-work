55e666fcd440a1e48f4f163ccc5a69f1c14451af
Badboy17838 <76719136+Badboy17838@users.noreply.github.com>
2020-12-29 05:53:18 +0100

    Apma

---
+++ app/src/main/java/com/example/android/apma/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.apma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView btn=findViewById(R.id.SingUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUp.class));
+            }
+        });
+    }
+}
